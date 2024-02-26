a72bd53d6edc379d60068ae06e41645065c0fdc9
jashpatel0799 <jashpatel0799@gmail.com>
2024-02-27 04:20:25 +0530

    Model training and Evaluation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ sys.path.append(abspath(dirname(dirname("MLOps_Project"))))
 from src.exception import CustomException
 from src.logger import logging
 from src.components.data_transformation import DataTransformation, DataTransformationConfig
+from src.components.model_trainer import ModelTrainer, ModelTrainerConfig
+
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
@@ -54,4 +56,8 @@ if __name__ == "__main__":
     train_data, test_data = obj.initiate_data_ingestion()
     
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_array, test_array, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+    
+    modeltrainer = ModelTrainer()
+    
+    print(modeltrainer.initiate_model_trainer(train_array, test_array))
